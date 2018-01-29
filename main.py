4aceeada5754eae8acad2e64a178830fa10d6878
Javier Abadía <javi.abadia@gmail.com>
2018-01-29 19:42:51 +0100

    remove path things

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from pathlib import Path
-
-import os
-
 from parse import parse
 from solve_javi import solve
 from utils import is_valid_slice
@@ -10,10 +6,7 @@ from writer import write, Slice
 from solve_javi import solve
 
 if __name__ == '__main__':
-    ROOT_DIR = Path(os.path.abspath(__file__)).parent
-    filename = os.path.join(ROOT_DIR, "input_files", "input1.txt")
-
-    world = parse(input_file=filename)
+    world = parse(input_file='./input_files/input1.txt')
 
     # assert not is_valid_slice(world, Slice(0, 0, 0, 0))
     # assert is_valid_slice(world, Slice(0, 0, 1, 1))
