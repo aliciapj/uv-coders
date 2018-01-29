7e8c78b79ca53571e63558d22fca138bf2922846
Javier Abadía <javi.abadia@gmail.com>
2018-01-29 22:44:42 +0100

    simplified code

--- parse.py
+++ parse.py
@@ -1,11 +1,7 @@
 def parse(input_file):
-
     with open(file=input_file, mode='r') as f:
         rows, cols, min_of_each_ingredient, max_cells = map(int, f.readline().strip().split(' '))
-        pizza = []
-        for row in range(rows):
-            line = f.readline().strip()
-            pizza.append(list(line))
+        pizza = [list(f.readline().strip()) for _ in range(rows)]
 
         world = {
             'min_of_each_ingredient': min_of_each_ingredient,
