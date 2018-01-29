a936e6ec236ac15896110613bb1a219230d7273f
Javier Abadía <javi.abadia@gmail.com>
2018-01-29 18:27:18 +0100

    main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from parse import parse
 from writer import write, Slice
-
+from utils import print_pizza
 
 world = {
     'min_of_each_ingredient': 1,
@@ -13,16 +13,39 @@ world = {
 }
 
 
-
+def find_slice(world, row, col):
+    return Slice(row, col, row, col)
 
 
 def solve(world):
+    solution = []
 
-    # search for first available ingredient
+    while True:
+        # search for first available ingredient
+        row_count = len(world['pizza'])
+        col_count = len(world['pizza'][0])
+        for row in range(row_count):
+            for col in range(col_count):
+                if world['pizza'][row][col] != 'X':
+                    break
+        if row == row_count and col == col_count:
+            # don't have any available cells
+            return solution
 
-    # try to find a suitable slice
+        # try to find a suitable slice starting at row, col
+        slice = find_slice(world, row, col)
 
-    # add to list of slices
+        if slice:
+            for row in range(slice.row_init, slice.row_end+1):
+                for col in range(slice.col_init, slice.col_end+1):
+                    world['pizza'][row][col] = 'X'
+            # add to list of slices
+            solution.append(slice)
+        else:
+            world['pizza'][row][col] = 'X'
+
+        print_pizza(world)
+
     return solution
 
 
