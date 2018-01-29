4b908575d3e166c88e40ac1d054963621156691d
rubnvp <rubn.vp@gmail.com>
2018-01-29 17:56:35 +0100

    based world object

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import parse from parse
 import write from writer
 
+world = {
+    min_cells: 1,
+    max_cells: 6,
+    pizza: [
+        ['T','T','T','T','T'],
+        ['T','M','M','M','T'],
+        ['T','T','T','T','T'],
+    ],
+}
+
 
 def solve(world):
     return "SOLVED!"
 
 
 if __name__ == '__main__':
-    world = parse('input_files/input1.txt')
+    # world = parse('input_files/input1.txt')
 
     solution = solve(world)
 
