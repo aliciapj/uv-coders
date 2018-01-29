0611d60af835fd3149709a480df9f7efbba83eca
alicia <apj.ali@gmail.com>
2018-01-29 18:57:09 +0100

    Slice len to utils

--- utils.py
+++ utils.py
@@ -4,3 +4,7 @@ def print_pizza(world):
     print('-')
     for row in world['pizza']:
         print(''.join(row))
+
+
+def get_slice_len(slice):
+    return (slice.row_end - slice.row_init) * (slice.column_end - slice.column_init)
