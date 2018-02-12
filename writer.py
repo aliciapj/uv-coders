9c4d6f2fb499a120d5e0a1e835f32299d8c31303
rubnvp <rubn.vp@gmail.com>
2018-02-12 17:49:54 +0100

    added writer

--- writer.py
+++ writer.py
@@ -1,9 +1,20 @@
 # writer.py
+from sets import Set
+
+solution_test = { # cache_id -> video_ids
+    0: Set([2]),
+    1: Set([3, 1]),
+    2: Set([0, 1]),
+}
+
 def write(solution, output_file):
     print('writing to', output_file)
 
     with open(output_file, 'w') as f:
-        # f.write('%d\n' % (len(solution),))
-        # for slice in solution:
-        #     f.write('%d %d %d %d\n' % (slice.row_init, slice.col_init, slice.row_end, slice.col_end))
+        f.write('%d\n' % (len(solution),))
+        for cache_id, video_ids in solution.iteritems():
+            f.write('%s %s\n' % (cache_id, ' '.join(str(video_id) for video_id in video_ids)))
         pass
+
+
+# write(solution_test, 'test1.txt')
