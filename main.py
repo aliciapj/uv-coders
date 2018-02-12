91f0673d3b106e06c02f3f3daea7e5630c7528c8
Javier Abadía <javi.abadia@gmail.com>
2018-02-12 20:02:59 +0100

    measure execution time

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import time
+
 from parse import parse
 from utils import get_output_file, calculate_score
 from writer import write
@@ -22,8 +24,13 @@ def process_file(solve, input_file):
     output_file = get_output_file(input_file)
 
     world = parse(input_file=os.path.join('./input_files', input_file))
+    t0 = time.time()
     solution = solve(world)
+    t1 = time.time()
+    print("solution took %.1f sec" % (t1-t0,))
     score = calculate_score(world, solution)
+    t2 = time.time()
+    print("calculate score took %.1f sec" % (t2-t1,))
     print("SCORE: %d" % score)
     write(solution, output_file)
     return score
@@ -34,8 +41,11 @@ if __name__ == '__main__':
         print("python3 needed")
         sys.exit(1)
 
+    t0 = time.time()
     total_score = 0
     for input_file in config['input_files']:
         total_score += process_file(config['solve'], input_file)
 
     print('TOTAL SCORE: %d' % total_score)
+    t1 = time.time()
+    print("TOTAL TIME: %.1f sec" % (t1-t0,))
