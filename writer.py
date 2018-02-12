73cb447fff9ecb45b09b46365ef7f11a6eaf7326
alicia <apj.ali@gmail.com>
2018-02-12 18:38:08 +0100

    Very first solution

--- writer.py
+++ writer.py
@@ -11,7 +11,7 @@ def write(solution, output_file):
 
     with open(output_file, 'w') as f:
         f.write('%d\n' % (len(solution),))
-        for cache_id, video_ids in solution.iteritems():
+        for cache_id, video_ids in solution.items():
             f.write('%s %s\n' % (cache_id, ' '.join(str(video_id) for video_id in video_ids)))
         pass
 
