68742ce3bf82cda65e8e750024bd53a56a771b36
Javier Abadía <javi.abadia@gmail.com>
2018-02-12 17:51:56 +0100

    changes a bit the format

--- parse.py
+++ parse.py
@@ -1,22 +1,19 @@
 # parse.py
 def parse(input_file):
     with open(file=input_file, mode='r') as f:
-        rows, cols, min_of_each_ingredient, max_cells = map(int, f.readline().strip().split(' '))
-        # pizza = [list(f.readline().strip()) for _ in range(rows)]
-
         world = {
             'cache_size': 100,
             'caches': [100, 100, 100],
             'videos': [50, 50, 80, 30, 110],  # array de MBs
-            'endpoints': [   # datacenter_latency: number, cache_latency: {id_cache: latency}
+            'endpoints': [  # datacenter_latency: number, cache_latency: {id_cache: latency}
                 {'datacenter_latency': 1000,
-                 'cache_latency': [
-                     {0: 100},
-                     {2: 200},
-                     {1: 300}
-                 ]},
+                 'cache_latency': {
+                     0: 100,
+                     2: 200,
+                     1: 300
+                 }},
                 {'datacenter_latency': 500,
-                 'cache_latency': []}
+                 'cache_latency': {}}
             ],
             'requests': [  # video_id, endpoint_id, count
                 {'endpoint': 0,
