f58472874df5fa9bd6a3615f59d092427b4d377f
alicia <apj.ali@gmail.com>
2018-02-12 17:31:47 +0100

    Parser v1 - manual info

--- parse.py
+++ parse.py
@@ -5,6 +5,31 @@ def parse(input_file):
         # pizza = [list(f.readline().strip()) for _ in range(rows)]
 
         world = {
+            'videos': [50, 50, 80, 30, 110],  # array de MBs
+            'endpoints': [   # datacenter_latency: number, cache_latency: {id_cache: latency}
+                {'datacenter_latency': 1000,
+                 'cache_latency': [
+                     {0: 100},
+                     {2: 200},
+                     {1: 300}
+                 ]},
+                {'datacenter_latency': 500,
+                 'cache_latency': []}
+            ],
+            'requests': [  # video_id, endpoint_id, count
+                {'endpoint': 0,
+                 'video': 3,
+                 'count': 1500},
+                {'endpoint': 1,
+                 'video': 0,
+                 'count': 1000},
+                {'endpoint': 0,
+                 'video': 4,
+                 'count': 500},
+                {'endpoint': 0,
+                 'video': 1,
+                 'count': 1000}
+            ],
         }
 
     return world
