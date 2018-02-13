c652fdab700b3cc7e00bf9356da4a3013b057062
Javier Abadía <javi.abadia@gmail.com>
2018-02-13 09:44:13 +0100

    auto format and one comment

--- videos/utils.py
+++ videos/utils.py
@@ -17,20 +17,24 @@ TOO_BIG_LATENCY = 1000000
 
 
 def calculate_score(world, solution):
-
     # first check that cache capacity has not been exceeded
     for cache_id, videos in solution.items():
         size_of_videos_in_cache = sum(world['videos'][video_id] for video_id in videos)
         if size_of_videos_in_cache > world['caches'][cache_id]:
-            print("BAD SOLUTION: cache %d contains videos with %d Mb > %d Mb" % (cache_id, size_of_videos_in_cache, world['caches'][cache_id]))
+            print("BAD SOLUTION: cache %d contains videos with %d Mb > %d Mb" % (
+                cache_id, size_of_videos_in_cache, world['caches'][cache_id])
+                  )
 
+    # now, calculate the score
     score = 0
     total_requests = 0
     for request in world['requests']:
         endpoint = world['endpoints'][request['endpoint']]
         caches_containing_video = [cache_id for (cache_id, videos) in solution.items() if request['video'] in videos]
-        min_latency = min((endpoint['cache_latency'].get(cache, TOO_BIG_LATENCY) for cache in caches_containing_video),
-                          default=None)
+        min_latency = min(
+            (endpoint['cache_latency'].get(cache, TOO_BIG_LATENCY) for cache in caches_containing_video),
+            default=TOO_BIG_LATENCY
+        )
         latency_to_data_center = endpoint['datacenter_latency']
 
         if min_latency and min_latency < latency_to_data_center:
