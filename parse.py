fe8e03d0c2c928f3dbdf1397efb3b61a175024c1
Javier Abadía <javi.abadia@gmail.com>
2018-02-13 09:29:15 +0100

    easier to swap test data in and out

--- parse.py
+++ parse.py
@@ -40,34 +40,34 @@ def parse(input_file):
                 }
             )
 
-        # world = {
-        #     'cache_size': 100,
-        #     'caches': [100, 100, 100],
-        #     'videos': [50, 50, 80, 30, 110],  # array de MBs
-        #     'endpoints': [   # datacenter_latency: number, cache_latency: {id_cache: latency}
-        #         {'datacenter_latency': 1000,
-        #          'cache_latency': {
-        #              0: 100,
-        #              2: 200,
-        #              1: 300,
-        #          }},
-        #         {'datacenter_latency': 500,
-        #          'cache_latency': {}}
-        #     ],
-        #     'requests': [  # video_id, endpoint_id, count
-        #         {'endpoint': 0,
-        #          'video': 3,
-        #          'count': 1500},
-        #         {'endpoint': 1,
-        #          'video': 0,
-        #          'count': 1000},
-        #         {'endpoint': 0,
-        #          'video': 4,
-        #          'count': 500},
-        #         {'endpoint': 0,
-        #          'video': 1,
-        #          'count': 1000}
-        #     ],
-        # }
+        world_test = {
+            'cache_size': 100,
+            'caches': [100, 100, 100],
+            'videos': [50, 50, 80, 30, 110],  # array de MBs
+            'endpoints': [   # datacenter_latency: number, cache_latency: {id_cache: latency}
+                {'datacenter_latency': 1000,
+                 'cache_latency': {
+                     0: 100,
+                     2: 200,
+                     1: 300,
+                 }},
+                {'datacenter_latency': 500,
+                 'cache_latency': {}}
+            ],
+            'requests': [  # video_id, endpoint_id, count
+                {'endpoint': 0,
+                 'video': 3,
+                 'count': 1500},
+                {'endpoint': 1,
+                 'video': 0,
+                 'count': 1000},
+                {'endpoint': 0,
+                 'video': 4,
+                 'count': 500},
+                {'endpoint': 0,
+                 'video': 1,
+                 'count': 1000}
+            ],
+        }
 
     return world
