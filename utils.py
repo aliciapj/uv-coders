82f3875e902bfffb5f655058cc469f3175026727
Javier Abadía <javi.abadia@gmail.com>
2018-02-13 09:30:42 +0100

    more consistent file names

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_output_file(input_file):
-    output_file = os.path.basename(os.path.splitext(input_file)[0]) + '_solution.txt'
+    output_file = os.path.basename(os.path.splitext(input_file)[0]) + '.out'
     output_file = os.path.join('./output_files', output_file)
     return output_file
 
