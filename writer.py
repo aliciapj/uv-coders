375eac8061c6d35ee189a774dee12497fbf55110
alicia <apj.ali@gmail.com>
2018-02-19 18:24:01 +0100

    Writer - first version

--- writer.py
+++ writer.py
@@ -3,7 +3,23 @@ def write(solution, output_file):
     print('writing to', output_file)
 
     with open(output_file, 'w') as f:
-        # f.write('%d\n' % (len(solution),))
-        # for cache_id, video_ids in solution.items():
-        #     f.write('%s %s\n' % (cache_id, ' '.join(str(video_id) for video_id in video_ids)))
-        pass
+
+        for server_index, server_info in enumerate(solution['servers']):
+            out_server = {}
+
+            for row_index, row_info in enumerate(solution['rows']):
+                try:
+                    location_in_row = row_info.index(server_index)
+                    out_server['row'] = row_index
+                    out_server['slot'] = location_in_row
+                except:
+                    pass
+
+            for pool_index, pool_info in enumerate(solution['pools']):
+                if server_index in pool_info:
+                    out_server['pool'] = pool_index
+
+            if out_server:
+                f.write('%d %d %d\n' % (out_server['row'], out_server['slot'], out_server['pool']))
+            else:
+                f.write('x\n')
