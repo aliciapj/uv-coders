773e19f9a42c342e4b15a4b9f6a92e29ecc78ded
Javier Abadía <javi.abadia@gmail.com>
2018-02-19 17:31:27 +0100

    añado soporte para unit testing

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+from utils import calculate_score
+
+
+class TestCalculateScore(unittest.TestCase):
+
+    def test_calculate_score(self):
+        self.assertEqual(True, False, 'ha fallado el test!')
+
