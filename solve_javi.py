0b996df5a2535f0e82e19a99787fa07272f8c890
Javier Abadía <javi.abadia@gmail.com>
2018-03-01 20:54:53 +0100

    prints with conditional

--- solve_javi.py
+++ solve_javi.py
@@ -8,6 +8,8 @@ import heapq
 
 Car = namedtuple('Car', 'busy_until id pos')
 
+DEBUG = False
+
 def solve(world):
     cars = [Car(-1, i, Position(0, 0)) for i in range(world['n_cars'])]  # coches ordenados por tiempo en el que se van a quedar libres
     rides = sorted(world['rides'], key=itemgetter('latest_start'))  # rides ordenadas por latest_start
@@ -16,7 +18,8 @@ def solve(world):
     t = 0
     while t < world['steps'] and len(rides):
         # mirar los coches que están libres
-        print('instante %d' % (t,))
+        if DEBUG:
+            print('instante %d' % (t,))
         first_car = cars[0]
         if cars[0].busy_until < t:
             # para cada coche mirar los rides que podría atender, asignarle uno
@@ -28,9 +31,10 @@ def solve(world):
                     car = heapq.heappop(cars)
                     del rides[ride_pos]
 
-                    print('asignando ride', ride)
-                    print('a', car)
-                    print('quedan %d rides por asignar' % (len(rides),))
+                    if DEBUG:
+                        print('asignando ride', ride)
+                        print('a', car)
+                        print('quedan %d rides por asignar' % (len(rides),))
 
                     ride['real_start'] = t + car_to_start
                     ride['real_finish'] = t + car_to_start + ride['duration']
@@ -41,7 +45,8 @@ def solve(world):
                     heapq.heappush(cars, car_after)
 
         if first_car == cars[0]:  # couldn't assign anything to this car... get rid of it
-            print("getting rid of car %d" % (cars[0].id,))
+            if DEBUG:
+                print("getting rid of car %d" % (cars[0].id,))
             heapq.heappop(cars)
 
         t += cars[0].busy_until + 1  # avanzo la simulación hasta que el próximo coche se quede libre
