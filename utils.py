5bb30a1157a7c21fb028264062d86f9026ec1783
alicia <apj.ali@gmail.com>
2018-03-01 19:30:06 +0100

    Get score: first version

--- utils.py
+++ utils.py
@@ -13,5 +13,19 @@ def analyze_world(world):
 
 
 def calculate_score(world, solution):
+    # Earns points: (distance) + (bonus)
+    # Each ride completed before its latest finish earns the number of points equal
+    # to the distance between the start intersection and the finish intersection.
+    # Additionally, each ride which started exactly in its earliest allowed start step
+    # gets an additional timeliness bonus of B.
+
     score = 0
+    for ride in solution['rides']:
+        distance = ride['duration']
+
+        bonus_finish = ride['finish'] - ride['real_finish'] if ride['real_finish'] < ride['finish'] else 0
+        bonus_start = solution['bonus'] if ride['real_start'] == ride['start'] else 0
+
+        score += distance + bonus_finish + bonus_start
+
     return score
