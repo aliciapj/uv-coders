cdbb9eedcfed319223cd3cffbc8f3e1f7a7627f6
alicia <apj.ali@gmail.com>
2018-03-01 19:13:43 +0100

    Writer: first version

--- writer.py
+++ writer.py
@@ -3,5 +3,18 @@ def write(solution, output_file):
     print('writing to', output_file)
 
     with open(output_file, 'w') as f:
-        # f.write('%d %d %d\n' % (out_server['row'], out_server['slot'], out_server['pool']))
-        pass
+
+        for vehicle in solution['vehicles']:
+            rides = [str(ride['id']) for ride in vehicle['rides']]
+            f.write('{} {}\n'.format(len(vehicle['rides']), ' '.join(rides)))
+
+if __name__ == '__main__':
+
+    w = {
+        'vehicles': [
+            {'rides': [{'id': 0}]},
+            {'rides': [{'id': 1},{'id': 2}]}
+        ]
+    }
+
+    write(w, '/home/alicia/workspace/uv-coders/output.txt')
