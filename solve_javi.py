5faf38f732360e693093bfbf8c8920d391bf269a
Javier Abadía <javi.abadia@gmail.com>
2018-03-01 20:24:37 +0100

    keep working in solve

--- solve_javi.py
+++ solve_javi.py
@@ -10,7 +10,6 @@ Car = namedtuple('Car', 'busy_until id pos')
 
 def solve(world):
 
-
     cars = [Car(-1, i, Position(0, 0)) for i in range(world['n_cars'])]  # coches ordenados por tiempo en el que se van a quedar libres
     rides = sorted(world['rides'], key=itemgetter('latest_start'))  # rides ordenadas por latest_start
     solution = defaultdict(list)
@@ -18,24 +17,31 @@ def solve(world):
     t = 0
     while t < world['steps'] and len(rides):
         # mirar los coches que están libres
-        while cars[0].busy_until < t:
+        print('instante %d' % (t,))
+        if cars[0].busy_until < t:
             # para cada coche mirar los rides que podría atender, asignarle uno
             for ride_pos, ride in enumerate(rides):
                 car = cars[0]
                 car_to_start = distance(car.pos, ride['start'])
-                if t + car_to_start <= ride['latest_start']:   # TODO: < o <= ?
+                if ride['earliest_start'] <= t + car_to_start <= ride['latest_start']:   # TODO: < o <= ?
                     # asignar la ride al coche
                     car = heapq.heappop(cars)
                     del rides[ride_pos]
 
+                    print('asignando ride', ride)
+                    print('a', car)
+                    print('quedan %d rides por asignar' % (len(rides),))
+
                     ride['real_start'] = t + car_to_start
-                    ride['real_finish'] = t + car_to_start
+                    ride['real_finish'] = t + car_to_start + ride['duration']
                     solution[car.id].append(ride)
 
                     # devolverlo a la lista
-                    car_after = Car(busy_until=t + car_to_start + ride['duration'], id=car.id, pos=ride['finish'])
+                    car_after = Car(busy_until=ride['real_finish'], id=car.id, pos=ride['finish'])
                     heapq.heappush(cars, car_after)
 
-        t = cars[0].busy_until+1  # avanzo la simulación hasta que el próximo coche se quede libre
+        t += cars[0].busy_until + 1  # avanzo la simulación hasta que el próximo coche se quede libre
 
-    return solution
+    print('SOLUTION')
+    print(dict(solution))
+    return dict(solution)
