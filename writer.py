6d89931c0a7756fdbed8f492583da2ca48b2de6f
Javier Abadía <javi.abadia@gmail.com>
2018-03-01 20:34:31 +0100

    fix writer

--- writer.py
+++ writer.py
@@ -4,17 +4,16 @@ def write(solution, output_file):
 
     with open(output_file, 'w') as f:
 
-        for vehicle in solution['vehicles']:
-            rides = [str(ride['id']) for ride in vehicle['rides']]
-            f.write('{} {}\n'.format(len(vehicle['rides']), ' '.join(rides)))
+        for car, rides in solution.items():
+            f.write('{} {}\n'.format(len(rides), ' '.join(str(ride['id']) for ride in rides)))
 
-if __name__ == '__main__':
-
-    w = {
-        'vehicles': [
-            {'rides': [{'id': 0}]},
-            {'rides': [{'id': 1},{'id': 2}]}
-        ]
-    }
-
-    write(w, '/home/alicia/workspace/uv-coders/output.txt')
+# if __name__ == '__main__':
+#
+#     w = {
+#         'vehicles': [
+#             {'rides': [{'id': 0}]},
+#             {'rides': [{'id': 1},{'id': 2}]}
+#         ]
+#     }
+#
+#     write(w, '/home/alicia/workspace/uv-coders/output.txt')
